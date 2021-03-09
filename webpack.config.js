330e02aac629061eeb4cacad7cc37c0cea350700
张亚飞 <1739077662@qq.com>
2021-03-09 11:59:20 +0800

    add:webpack-bundle-analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,26 @@
-let path = require("path")
+let path = require("path");
+
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
+  .BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: path.resolve(__dirname, "src/index.tsx"),
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "bundle.js",
-    },
-    mode: "development",
-    module: {
-        rules: [
-          {
-            test: /\.(tsx?)/i,
-            use: ["babel-loader"],
-          },
-        ],
+  entry: path.resolve(__dirname, "src/index.tsx"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(tsx?)/i,
+        use: ["babel-loader"],
       },
-      resolve: {
-        extensions: ['.ts', '.tsx', '.js'],
-      },
-  }
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+
+  plugins: [new BundleAnalyzerPlugin({})],
+};
