7e3dfd58b99b0b681cbc5c4b7eaf4e9fb12e0dc1
张亚飞 <1739077662@qq.com>
2021-03-29 10:51:38 +0800

    effectHook

--- src/hook/App.tsx
+++ src/hook/App.tsx
@@ -1,17 +1,30 @@
 import React, { Component } from "react";
+import EffectView from "./EffectView";
 import StateView from "./StateView";
 
 interface Props {}
 interface State {}
 
 class App extends Component<Props, State> {
-  state = {};
+  state = { isShow: true };
 
   render() {
     return (
       <>
-        <p>hook</p>
+        <p>
+          hook{" "}
+          <button
+            onClick={() => {
+              this.setState({
+                isShow: !this.state.isShow,
+              });
+            }}
+          >
+            chang show
+          </button>
+        </p>
         <StateView />
+        {this.state.isShow && <EffectView />}
       </>
     );
   }
---
+++ src/hook/EffectView.tsx
@@ -0,0 +1,27 @@
+import React, { ReactElement, useEffect, useState } from "react";
+
+interface Props {}
+
+export default function EffectView({}: Props): ReactElement {
+  const [count, setCount] = useState({ num: 0 });
+  useEffect(() => {
+    console.log("effect");
+    // 代码一
+    return () => {
+      console.log("effect return");
+      //   代码二
+    };
+  }, [8]);
+  return (
+    <div>
+      effect hook {count.num}
+      <button
+        onClick={() => {
+          setCount({ num: count.num + 1 });
+        }}
+      >
+        click
+      </button>
+    </div>
+  );
+}
