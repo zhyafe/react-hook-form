7e26337d8a3b21c216c573d7191077ff8c9d6133
张亚飞 <1739077662@qq.com>
2021-04-06 13:38:20 +0800

    使用yup校验

--- src/demo/App.tsx
+++ src/demo/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
-import BaseUse from "./baseUse";
+import BaseUse from "./BaseUse";
 import Control from "./Control";
+import ControlYup from "./ControlYup";
 
 function App() {
   return (
     <div>
       <p>base use -----------------------</p>
       <BaseUse />
-      <p> -----------------------</p>
+      <p> control -----------------------</p>
       <Control />
+      <p> controlYup -----------------------</p>
+      <ControlYup />
     </div>
   );
 }
---
+++ src/demo/ControlYup.tsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+interface Props {}
+
+function Input(field) {
+  // console.log("props");
+
+  return (
+    <div>
+      {/* <p onClick={() => onChange(Math.floor(Math.random() * 10))}>onChagne</p>
+      <p>
+        {name}:{value}
+      </p> */}
+      <input autoComplete="off" {...field}></input>
+    </div>
+  );
+}
+
+const schema = yup.object().shape({
+  name: yup.string().required().min(9),
+  age: yup.number().positive().integer().required(),
+});
+
+const ControlYup = (props: Props) => {
+  const { control, handleSubmit } = useForm({
+    resolver: yupResolver(schema),
+  });
+  return (
+    <div>
+      <Controller
+        control={control}
+        name="name"
+        rules={{ required: true, minLength: 9 }}
+        render={({ field: { ref, ...resFiled }, fieldState, formState }) => {
+          console.log("control data", fieldState.error, formState.errors);
+
+          return (
+            <div>
+              <Input {...resFiled} />
+              {fieldState.error && <p>{fieldState.error.message}</p>}
+            </div>
+          );
+        }}
+      ></Controller>
+      <button
+        onClick={handleSubmit((data) => {
+          // console.log("submit data", data);
+        })}
+      >
+        提交
+      </button>
+    </div>
+  );
+};
+
+export default ControlYup;
