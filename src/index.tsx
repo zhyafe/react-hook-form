59ff5e956f2ad78d8a63fb3330d883f5dd39f4a1
张亚飞 <1739077662@qq.com>
2021-04-06 10:21:50 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import ReactDom from "react-dom";
 // import App from './context/App'
 // import App from "./modifyProps/App";
 // import App from "./hook/App";
-import App from "./api/App";
-// import App from "./demo/App";
+// import App from "./api/App";
+import App from "./demo/App";
 ReactDom.render(<App />, document.querySelector("#app"));
