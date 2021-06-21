43023147de9fe4fdf7c2dba16835eef32dd86132
张亚飞 <1739077662@qq.com>
2021-06-21 18:05:26 +0800

    feat:validation

--- src/2validation/App.tsx
+++ src/2validation/App.tsx
@@ -18,13 +18,31 @@ function App({}: Props): ReactElement {
     <div>
       <p>react-hook-form 基本使用</p>
       <form onSubmit={handleSubmit(submit)}>
+        <label htmlFor="phone">phone</label>
         <input
           type="text"
-          {...register("phone", { required: true })}
+          {...register("phone", {
+            required: true,
+            minLength: 5,
+            maxLength: 10,
+          })}
           autoComplete="off"
         />
         <p>{errors.phone && (errors.phone.message || errors.phone.type)}</p>
-        <br />
+        <label htmlFor="age">age</label>
+        <input
+          type="number"
+          {...register("age", { required: true, min: 6, max: 33 })}
+          autoComplete="off"
+        />
+        <p>{errors.age && (errors.age.message || errors.age.type)}</p>
+        <label htmlFor="name">name</label>
+        <input
+          type="text"
+          {...register("name", { required: true, pattern: /^\w{2,5}$/ })}
+          autoComplete="off"
+        />
+        <p>{errors.name && (errors.name.message || errors.name.type)}</p>
         <button type="submit">提交</button>
       </form>
     </div>
--- src/2validation/doc.md
+++ src/2validation/doc.md
@@ -1,7 +1,15 @@
 ## 表单校验
 
 - 添加校验
-  register 函数在调用时可传入第二个参数，进行校验规则的设定
+  register 函数在调用时可传入第二个参数，进行校验规则的设定;值和 html 的校验相似
+  可设置的值有
+  - required
+  - min
+  - max
+  - minLength
+  - maxLength
+  - pattern
+  - validate
 
 ```js
 register("phone", { required: true });
